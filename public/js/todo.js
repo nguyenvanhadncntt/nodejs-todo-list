bdc2bd5ea0b0e370b818b78b85bb17ef8ce76e4f
nguyenvanhadncntt <nguyenvanhadncntt@gmail.com>
2020-09-29 10:48:51 +0700

    update todo page

--- public/js/todo.js
+++ public/js/todo.js
@@ -6,12 +6,27 @@ var todoId = $("#todoId");
 var dataList;
 function loadData() {
     $.ajax({url: `${serverApi}/todos`, success: function(result){
-        $('#tbody').html('');
+        $('#content').html('');
         dataList = result;
         for (let i = 0; i < dataList.length; i++) {
-            $('#tbody').append('<tr><td>' + dataList[i].title + '</td><td>' + dataList[i].description + '</td><td>' + dataList[i].createdAt + '</td><td colspan="2">' + createRemoveButton(dataList[i].id)) +'</td>';
+            $('#content').append("<div class='col-8 mx-auto todo-card row mb-3'><div><span class='title'>title: "
+                + dataList[i].title + "</span><span class='description'>Description: "
+                + dataList[i].description + "</span><span class='description'>Created Date: "
+                + formatDate(dataList[i].createdAt) + "</span></div><div class='col-2 ml-auto icon-section'>"
+                + createRemoveButton(dataList[i].id)) +"</div>";
         }
     }});
+}
+
+formatDate = (date) => {
+    if (typeof date === 'string') {
+        date = new Date(date);
+    }
+    let dd = String(date.getDate()).padStart(2, '0');
+    let mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = date.getFullYear();
+
+    return mm + '/' + dd + '/' + yyyy;
 }
 
 deleteTodo = (id) => {
@@ -21,7 +36,15 @@ deleteTodo = (id) => {
 }
 
 createRemoveButton = (id) => {
-    return '<button class="btn btn-info mr-2" data-toggle="modal" data-target="#exampleModal" onclick="openUpdateTodo('+id+')">Update</button><button class="btn btn-danger" onclick="deleteTodo('+ id +')">Remove</button>';
+    return `
+    <div class="ml-auto icon-section">
+        <div class="icon mr-2 mt-1" onclick="openUpdateTodo(${id})" data-toggle="modal" data-target="#exampleModal">
+            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="edit" class="svg-inline--fa fa-edit fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M402.6 83.2l90.2 90.2c3.8 3.8 3.8 10 0 13.8L274.4 405.6l-92.8 10.3c-12.4 1.4-22.9-9.1-21.5-21.5l10.3-92.8L388.8 83.2c3.8-3.8 10-3.8 13.8 0zm162-22.9l-48.8-48.8c-15.2-15.2-39.9-15.2-55.2 0l-35.4 35.4c-3.8 3.8-3.8 10 0 13.8l90.2 90.2c3.8 3.8 10 3.8 13.8 0l35.4-35.4c15.2-15.3 15.2-40 0-55.2zM384 346.2V448H64V128h229.8c3.2 0 6.2-1.3 8.5-3.5l40-40c7.6-7.6 2.2-20.5-8.5-20.5H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V306.2c0-10.7-12.9-16-20.5-8.5l-40 40c-2.2 2.3-3.5 5.3-3.5 8.5z"></path></svg>
+        </div>
+        <div class="icon" onclick="deleteTodo(${id})">
+            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash-alt" class="svg-inline--fa fa-trash-alt fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M32 464a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128H32zm272-256a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zM432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z"></path></svg>
+        </div>
+    </div>`;
 };
 
 
